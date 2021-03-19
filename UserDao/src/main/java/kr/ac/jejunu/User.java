1ed154928a935fb28bb5bd0c007a93b7173905ef
xxria17 <xxrcy32@gmail.com>
2021-03-19 19:01:42 +0900

    0319 실습

---
+++ UserDao/src/main/java/kr/ac/jejunu/User.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ UserDao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+public class UserDao {
+    public User findById(Integer id) throws ClassNotFoundException, SQLException {
+        //데이터 Mysql 연결
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/xxria17?characterEncoding=utf-8&serverTimezone=UTC"
+                , "jeju", "jejupw");
+
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "select * from userinfo where id = ?"
+        );
+        preparedStatement.setInt(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+}
