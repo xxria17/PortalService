89d46d294c3aeae2e6714a7366b7afa4330dbb6a
xxria17 <xxrcy32@gmail.com>
2021-04-30 18:24:00 +0900

    0430 servlet

---
+++ UserDao/src/main/java/kr/ac/jejunu/UserServlet.java
@@ -0,0 +1,22 @@
+package kr.ac.jejunu;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class UserServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("*************** service *****************");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("*************** init *****************");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("*************** destroy *****************");
+    }
+}
