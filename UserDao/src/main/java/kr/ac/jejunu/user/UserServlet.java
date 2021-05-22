fab30747d4d3ca4c7c8fa741709b8d62dab0fe8b
xxria17 <xxrcy32@gmail.com>
2021-05-22 17:25:42 +0900

    0522 spring mvc

--- UserDao/src/main/java/kr/ac/jejunu/user/UserServlet.java
+++ UserDao/src/main/java/kr/ac/jejunu/user/UserServlet.java
@@ -1,12 +1,16 @@
-package kr.ac.jejunu;
+package kr.ac.jejunu.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
 
 import javax.servlet.*;
 import java.io.IOException;
 
+@Controller("/userServlet")
 public class UserServlet extends GenericServlet {
+    @Autowired
     private UserDao userDao;
 
     @Override
--- UserDao/src/main/webapp/user.jsp
+++ UserDao/src/main/webapp/user.jsp
@@ -1,8 +1,8 @@
 
-<%@ page import="kr.ac.jejunu.UserDao" %>
+<%@ page import="kr.ac.jejunu.user.UserDao" %>
 <%@ page import="org.springframework.context.ApplicationContext" %>
 <%@ page import="org.springframework.context.annotation.AnnotationConfigApplicationContext" %>
-<%@ page import="kr.ac.jejunu.User" %>
+<%@ page import="kr.ac.jejunu.user.User" %>
 <%@ page contentType="text/html; charset=UTF-8" %>
 
 <%
